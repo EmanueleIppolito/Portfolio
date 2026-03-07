074408933e5f11cce8717e17475fb4f48ca83e7b
EmanueleIppolito <emanuele2410@gmail.com>
2026-03-07 23:11:39 +0100

    Added filters

--- manga_library_api/backend/controllers/mangasController.js
+++ manga_library_api/backend/controllers/mangasController.js
@@ -1,89 +1,178 @@
-const mangas = require("../data/mangas.json")
+const mangas = require("../data/mangas.json");
 
-function index(req, res){
-    res.json(mangas)
+function index(req, res, next) {
+    try {
+        const { title, originalTitle, author, genre, tag, isOnGoing } = req.query;
+        let filteredMangas = mangas;
+        if (title) {
+            filteredMangas = filteredMangas.filter(manga =>
+                manga.title.toLowerCase()
+                    .includes(title.toLowerCase()))
+        }
+        if (originalTitle) {
+            filteredMangas = filteredMangas.filter(manga =>
+                manga.originalTitle.toLowerCase()
+                    .includes(originalTitle.toLowerCase()))
+        }
+        if (author) {
+            filteredMangas = filteredMangas.filter(manga =>
+                manga.author.toLowerCase()
+                    .includes(author.toLowerCase()))
+        }
+        if (genre) {
+            const requestedGenres = genre.split(",").map(singleGenre =>
+                singleGenre.toLowerCase().trim()
+            )
+            filteredMangas = filteredMangas.filter(manga => {
+                const matchedGenres = requestedGenres.filter(requestedGenre =>
+                    manga.genre.find(singleGenre =>
+                        singleGenre.toLowerCase() === requestedGenre))
+                return matchedGenres.length === requestedGenres.length
+            })
+
+        }
+        if (tag) {
+            const requestedTags = tag.split(",").map(singleTag =>
+                singleTag.toLowerCase().trim()
+            );
+
+            filteredMangas = filteredMangas.filter(manga => {
+                const matchedTags = requestedTags.filter(requestedTag =>
+                    manga.tag.find(singleTag =>
+                        singleTag.toLowerCase() === requestedTag
+                    )
+                );
+
+                return matchedTags.length === requestedTags.length;
+            });
+        }
+        if (isOnGoing !== undefined) {
+            const isOnGoingBool = isOnGoing === "true";
+            filteredMangas = filteredMangas.filter(manga =>
+                manga.isOnGoing === isOnGoingBool
+            )
+        }
+        res.json(filteredMangas);
+    } catch (err) {
+        next(err);
+    }
 }
 
-function show(req, res){
-const id = parseInt(req.params.id);
-    const manga = mangas.find(manga => manga.id === id);
-    if(!manga){
-        return res.status(404).json({error: "Manga non trovato"})
+function show(req, res, next) {
+    try {
+        const id = parseInt(req.params.id);
+        const manga = mangas.find(manga => manga.id === id);
+
+        if (!manga) {
+            return res.status(404).json({
+                error: "Manga non trovato"
+            });
+        }
+
+        res.json(manga);
+    } catch (err) {
+        next(err);
     }
-    res.json(manga)
 }
 
-function store(req, res){
-const body = req.body;
-    const newMangaId = mangas[mangas.length - 1].id + 1
-    const newManga = {
-        id: newMangaId,
-        ...body
-    }
+function store(req, res, next) {
+    try {
+        const body = req.body;
+        const newMangaId = mangas[mangas.length - 1].id + 1;
 
-    mangas.push(newManga)
-    res.status(201).json({
-        message: "Manga aggiunto con successo",
-        manga: newManga})
+        const newManga = {
+            id: newMangaId,
+            ...body
+        };
+
+        mangas.push(newManga);
+
+        res.status(201).json({
+            message: "Manga aggiunto con successo",
+            manga: newManga
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
-function update(req, res){
-const id = parseInt(req.params.id)
-    const body = req.body
-    const mangaIndex = mangas.findIndex(manga => manga.id === id)
-    
-    if(mangaIndex === -1){
-        return res.status(404).json({
-            error: "Manga non trovato"
-        })
+function update(req, res, next) {
+    try {
+        const id = parseInt(req.params.id);
+        const body = req.body;
+        const mangaIndex = mangas.findIndex(manga => manga.id === id);
+
+        if (mangaIndex === -1) {
+            return res.status(404).json({
+                error: "Manga non trovato"
+            });
+        }
+
+        const updatedManga = {
+            id,
+            ...body
+        };
+
+        mangas.splice(mangaIndex, 1, updatedManga);
+
+        return res.status(200).json({
+            message: `Il manga "${updatedManga.title}" è stato aggiornato`,
+            manga: updatedManga
+        });
+    } catch (err) {
+        next(err);
     }
-    const updatedManga = {
-        id,
-        ...body
-    }
-    mangas.splice(mangaIndex, 1, updatedManga)
-    return res.status(200).json({
-        message: `Il manga "${updatedManga.title}" è stato aggiornato`,
-        manga: updatedManga
-    })
 }
 
-function modify(req, res){
-const id = parseInt(req.params.id)
-    const body = req.body
-    const mangaIndex = mangas.findIndex(manga => manga.id === id)
+function modify(req, res, next) {
+    try {
+        const id = parseInt(req.params.id);
+        const body = req.body;
+        const mangaIndex = mangas.findIndex(manga => manga.id === id);
 
-    if(mangaIndex === -1){
-        return res.status(404).json({
-            error: "Manga non trovato"
-        })
+        if (mangaIndex === -1) {
+            return res.status(404).json({
+                error: "Manga non trovato"
+            });
+        }
+
+        const updatedManga = {
+            ...mangas[mangaIndex],
+            ...body,
+            id
+        };
+
+        mangas.splice(mangaIndex, 1, updatedManga);
+
+        res.status(200).json({
+            message: `Il manga "${updatedManga.title}" è stato modificato con successo`,
+            manga: updatedManga
+        });
+    } catch (err) {
+        next(err);
     }
-    const updatedManga = {
-        ...mangas[mangaIndex],
-        ...body,
-        id
-    }
-    mangas.splice(mangaIndex, 1, updatedManga)
-    res.status(200).json({
-        message: `Il manga "${updatedManga.title}" è stato modificato con successo`,
-        manga: updatedManga
-    })
 }
 
-function destroy(req, res){
-const id = parseInt(req.params.id)
-    const mangaIndex = mangas.findIndex(manga => manga.id === id)
+function destroy(req, res, next) {
+    try {
+        const id = parseInt(req.params.id);
+        const mangaIndex = mangas.findIndex(manga => manga.id === id);
 
-    if(mangaIndex === -1){
-        return res.status(404).json({
-            error: "Manga non trovato"
-        })
-    } 
-    const deletedManga = mangas[mangaIndex]
-    mangas.splice(mangaIndex, 1)
-    res.status(200).json({
-    message: `Il manga ${deletedManga.title} è stato eliminato`
-    })
+        if (mangaIndex === -1) {
+            return res.status(404).json({
+                error: "Manga non trovato"
+            });
+        }
+
+        const deletedManga = mangas[mangaIndex];
+        mangas.splice(mangaIndex, 1);
+
+        res.status(200).json({
+            message: `Il manga ${deletedManga.title} è stato eliminato`
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
@@ -93,4 +182,4 @@ module.exports = {
     update,
     modify,
     destroy
-}
+};
--- manga_library_api/backend/index.js
+++ manga_library_api/backend/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mangasRouter = require("./routes/mangasRouter.js")
 const app = express();
 app.use(express.json());
@@ -12,5 +11,12 @@ app.get("/", (req, res) => res.send("<h1>Benvenuto in Manga Library API</h1>"))
 app.use("/mangas", mangasRouter);
 
 app.use((req, res) => res.status(404).send("<h1> Ops... Rotta non trovata... 😣</h1>"))
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({
+        error: "Errore interno del server"
+    })
+})
 
 app.listen(port, () => console.log(`Server in ascolto su http://localhost:${port}`));
